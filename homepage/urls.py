4d3e92827725cc51e7481668ef3c163561ac5fb5
BENMULWA <mulwabenard9507@gmail.com>
2025-10-29 21:45:08 +0300

    Updated-logic

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('review--<str:token>/', views.review_detail, name='review_detail'), # the name review is the one that is mapped in the navbar on base.html and the link review--jpkeg>123 is the one that hundles the url that user sees after clicks 
+    path('about/', views.about_view, name='about'),
+    path('services/', views.services_view, name='services'),
+    path('careers/', views.careers_view, name='careers'),
     path('know_more/', views.know_more_view, name='know_more'),
     path('services/ecitizen/', views.ecitizen_detail, name='ecitizen_detail'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core import signing
+from .models import BlogPost
+
 
 def homepage(request):
     return render(request, 'homepage/home.html')
+
+def review_detail(request, token):
+    try:
+        post_id = signing.loads(token)
+        post = get_object_or_404(BlogPost, id=post_id)
+    except signing.BadSignature:
+        return render(request, '404.html', status=404)
+
+    return render(request, 'homepage/review.html', {'post': post})
+
+
+
+def about_view(request):
+    return render(request, 'homepage/about.html')
+
+def services_view(request):
+    return render(request, 'homepage/home.html')
+
+def careers_view(request):
+    return render(request, 'homepage/home.html')
+
 
 def know_more_view(request):
     return render(request, 'homepage/know_more.html')
